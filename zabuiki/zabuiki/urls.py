ab27c54ca6d37198f2dd6c117f312392924fef0c
a.zinoviev <a.zinoviev@sb117.ru>
2020-04-30 23:43:54 +0700

    add media, static urls

--- zabuiki/zabuiki/urls.py
+++ zabuiki/zabuiki/urls.py
@@ -17,14 +17,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
