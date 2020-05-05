42ff0252bca8370370f4364525877c23bb7eb3cc
a.zinoviev <a.zinoviev@sb117.ru>
2020-05-05 22:14:32 +0700

    add prod conf and docker

--- zabuiki/zabuiki/settings/prod.py
+++ zabuiki/zabuiki/settings/prod.py
@@ -1,11 +1,36 @@
 from .base import *
 
-MEDIA_URL = '/media/'
+DEBUG = False
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ['zabuiki.appspot.com', '127.0.0.1',]
+
+STATIC_URL = os.environ['STATIC_URL']
+
+MEDIA_URL = os.environ['MEDIA_URL']
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'zabuiki-static')
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'zabuiki-media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../../frontend/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD']
+    }
+}
+
+"""
+'NAME': 'zabuiki-db',
+'USER' : 'postgres',
+'PASSWORD' : 'Et142teqIFwOxpaz',
+"""
